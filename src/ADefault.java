5983c38e9369326e3faf597bdc0d112cc15fd23b
blortle <144735570+blortle-dev@users.noreply.github.com>
2025-05-20 15:07:05 -0500

    fire commit v13b 🔥

--- src/ADefault.java
+++ src/ADefault.java
@@ -1,3 +1,12 @@
 public class ADefault implements Ability {
+    /*
+
+    Abilities / Default
+
+    The player has no abilities.
+
+     */
+
     //literally all this class does is provide a default ability
+    //wowzers!
 }
---
+++ src/AFortress.java
@@ -0,0 +1,9 @@
+public class AFortress implements Ability {
+    /*
+
+    Abilities / Fortress
+
+    Pieces that have been stationary outside their starting area for 5 turns have an extra life after being jumped.
+
+     */
+}
